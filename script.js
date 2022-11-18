14d4a4ff9bf26235c4ad36187f592bfd07495ae5
dwnppo <80807440+dwnppo@users.noreply.github.com>
2022-11-18 18:01:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+alert("what if")
+alert("sabihin ko na gusto kita?")
+alert("what if")
+alert("gusto ko na ikaw maging kasama ko lagi?")
+alert("what if")
+alert("diretsuhin ko na nga")
+if (confirm("ako ba, gusto mo rin?")) {
+    alert("yie HAHAHAHAHHHAHAHAHHAHA")
+} else {
+    while (true){
+        if (confirm("sure ka na ba dyan?")) {
+            alert("yie HAHAHAHAHHHAHAHAHHAHAHAHHAHAH ")
+            break
+        } else {
+            alert("edi don't")
+        }
+    }
+    
+}
